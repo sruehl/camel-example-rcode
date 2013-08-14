eb22efc071947643e702d33bedc770b3e0001c2d
Christoph Emmersberger <cemmersb@googlemail.com>
2013-08-14 21:05:12 +0200

    + add Route Builder

---
+++ src/main/java/org/apacheextras/camel/examples/rcode/RCodeRouteBuilder.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.apacheextras.camel.examples.rcode;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ *
+ * @author cemmersb
+ */
+public class RCodeRouteBuilder extends RouteBuilder {
+
+  @Override
+  public void configure() throws Exception {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+  
+}
