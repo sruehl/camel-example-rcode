b15ba78bed9bb937c1c772f5f6800f7d240237c4
Christoph Emmersberger <cemmersb@googlemail.com>
2013-08-15 12:38:01 +0200

    + Configure routeBuilder + Add test data + Add logging dependencies + Add log configuration

--- src/main/java/org/apacheextras/camel/examples/rcode/RCodeRouteBuilder.java
+++ src/main/java/org/apacheextras/camel/examples/rcode/RCodeRouteBuilder.java
@@ -4,24 +4,37 @@
  */
 package org.apacheextras.camel.examples.rcode;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author cemmersb
  */
 public class RCodeRouteBuilder extends RouteBuilder {
-
+  
+  private static final Logger LOGGER = LoggerFactory.getLogger(RCodeRouteBuilder.class);
+  
   @Override
   public void configure() throws Exception {
     configureCsvRoute();
   }
-  
+
   private void configureCsvRoute() {
-    // TODO: Configure this route with meaningfull parameters
-    from("file://")
-        .marshal().csv()
-        .to("mock://testEndoint");
+    String path = System.getProperty("user.dir");
+    String url = "file://" + path + "/src/main/resources/data?noop=TRUE";
+    from(url)
+        .process(new Processor() {
+          @Override
+          public void process(Exchange exchng) throws Exception {
+            LOGGER.info("Exchange: {}", exchng);
+          }
+        })
+        .log("Unmarshalling CSV file.")
+        .unmarshal().csv()
+        .to("log:CSV?level=INFO");
   }
-  
 }
