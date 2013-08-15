cc18608f04cc229d54afbb997822c722cc21fb87
Sebastian Rühl <sebastian.ruehl.os>
2013-08-15 14:34:48 +0200

    + Improvements on file reading

--- src/main/java/org/apacheextras/camel/examples/rcode/Example.java
+++ src/main/java/org/apacheextras/camel/examples/rcode/Example.java
@@ -3,6 +3,8 @@ package org.apacheextras.camel.examples.rcode;
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
 
+import java.io.Console;
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -12,10 +14,19 @@ public class Example {
 
   public static void main(String... args) throws Exception {
     CamelContext camelContext = new DefaultCamelContext();
-    camelContext.addRoutes(new RCodeRouteBuilder());
+    if (args.length > 0) {
+      camelContext.addRoutes(new RCodeRouteBuilder(new File(args[0])));
+    } else {
+      camelContext.addRoutes(new RCodeRouteBuilder());
+    }
     camelContext.start();
-    //TODO: change to a better method
-    TimeUnit.SECONDS.sleep(5);
+    Console console = System.console();
+    if (console != null) {
+      console.printf("Please press enter to shutdown route.");
+      console.readLine();
+    } else {
+      TimeUnit.SECONDS.sleep(5);
+    }
     camelContext.stop();
   }
 }
--- src/main/java/org/apacheextras/camel/examples/rcode/RCodeRouteBuilder.java
+++ src/main/java/org/apacheextras/camel/examples/rcode/RCodeRouteBuilder.java
@@ -4,6 +4,7 @@
  */
 package org.apacheextras.camel.examples.rcode;
 
+import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
@@ -11,14 +12,35 @@ import org.apache.camel.dataformat.csv.CsvDataFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+
 /**
- *
  * @author cemmersb
  */
 public class RCodeRouteBuilder extends RouteBuilder {
-  
+
   private static final Logger LOGGER = LoggerFactory.getLogger(RCodeRouteBuilder.class);
-  
+
+  private File basePath;
+
+  public RCodeRouteBuilder() {
+    defaulBasePath();
+  }
+
+  public RCodeRouteBuilder(File basePath) {
+    this.basePath = basePath;
+  }
+
+  public RCodeRouteBuilder(CamelContext context) {
+    super(context);
+    defaulBasePath();
+  }
+
+  private void defaulBasePath() {
+    String path = System.getProperty("user.dir");
+    basePath = new File(path + "./rcode-example/data");
+  }
+
   @Override
   public void configure() throws Exception {
     configureCsvRoute();
@@ -29,17 +51,15 @@ public class RCodeRouteBuilder extends RouteBuilder {
     csv.setDelimiter(";");
     csv.setSkipFirstLine(true);
 
-    String path = System.getProperty("user.dir");
-    String url = "file://" + path + "/src/main/resources/data?noop=TRUE";
-    from(url)
-        .process(new Processor() {
-          @Override
-          public void process(Exchange exchng) throws Exception {
-            LOGGER.info("Exchange: {}", exchng);
-          }
-        })
-        .log("Unmarshalling CSV file.")
-        .unmarshal(csv)
-        .to("log:CSV?level=INFO");
+    from(basePath.toURI() + "?noop=TRUE")
+            .process(new Processor() {
+              @Override
+              public void process(Exchange exchange) throws Exception {
+                LOGGER.info("Exchange: {}", exchange);
+              }
+            })
+            .log("Unmarshalling CSV file.")
+            .unmarshal(csv)
+            .to("log:CSV?level=INFO");
   }
 }
